b796a157cd1ca44d149d9a438d07c3ecdebdfc73
jorditosa <jordisato88@gmail.com>
2023-12-15 21:09:07 +0100

    refactored cards endpoints

--- the-tarot-project/routes/cards.routes.js
+++ the-tarot-project/routes/cards.routes.js
@@ -3,18 +3,18 @@ const router = require('express').Router();
 const Card = require('../models/Card.model.js');
 
 // GET route to retrieve and display all the cards
-router.get('/cards', (req, res) => {
+router.get('/', (req, res) => {
   Card.find().then(allCards => {
     res.json(allCards);
     // res.render('cards/cards-list.hbs', { cards });
   })
 });
 
-router.get('/cards/create', (req, res) => {
+router.get('/create', (req, res) => {
   res.render('cards/cards-create.hbs');
 });
 
-router.post('/cards/create', (req, res) => {
+router.post('/create', (req, res) => {
   const { nameCard, categories, starSign, description } = req.body
   Card.create({ nameCard, categories, starSign, description }).then((newCards) => {
     // console.log(`Your Tarot card has been created`)
@@ -23,21 +23,21 @@ router.post('/cards/create', (req, res) => {
   });
 });
 
-router.get('cards/:id', (req, res) => {
+router.get('/:id', (req, res) => {
   const { id } = req.params;
   Card.findById(id).then(cardFromDB => {
     res.send(cardFromDB);
   });
 });
 
-router.get('cards/:id/edit', (req, res) => {
+router.get('/:id/edit', (req, res) => {
   const { id } = req.params;
   Card.findById(id).then(cardFromDB => {
     res.render('cards/cards-edit', { cardFromDB });
   });
 });
 
-router.post('cards/:id/edit', (req, res) => {
+router.post('/:id/edit', (req, res) => {
   const { nameCard, categories, starSign, description } = req.body
   const { id } = req.params;
   Card.findByIdAndUpdate(id, { nameCard, categories, starSign, description }, { new: true })
@@ -47,7 +47,7 @@ router.post('cards/:id/edit', (req, res) => {
     })
 });
 
-router.get('cards/:id/delete', (req, res, next) => {
+router.get('/:id/delete', (req, res, next) => {
   const { id } = req.params;
   Card.findByIdAndDelete(id)
     .then(() => {
@@ -55,7 +55,7 @@ router.get('cards/:id/delete', (req, res, next) => {
     }).catch(err => next(err));
 });
 
-router.post('cards/:id/delete', (req, res, next) => {
+router.post('/:id/delete', (req, res, next) => {
   const { id } = req.params;
   Card.findByIdAndDelete(id)
     .then(() => {
