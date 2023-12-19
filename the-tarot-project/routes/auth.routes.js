61e2d332a2a2915e73327ee4938794bf1dfc8448
jorditosa <jordisato88@gmail.com>
2023-12-19 22:10:39 +0100

    fixing signup model for registration

--- the-tarot-project/routes/auth.routes.js
+++ the-tarot-project/routes/auth.routes.js
@@ -22,13 +22,13 @@ router.get("/signup", isLoggedOut, (req, res) => {
 
 // POST /auth/signup
 router.post("/signup", isLoggedOut, (req, res) => {
-  const { username, email, password } = req.body;
+  const { name, email, password } = req.body;
 
-  // Check that username, email, and password are provided
-  if (username === "" || email === "" || password === "") {
+  // Check that name, email, and password are provided
+  if (name === "" || email === "" || password === "") {
     res.status(400).render("auth/signup", {
       errorMessage:
-        "All fields are mandatory. Please provide your username, email and password.",
+        "All fields are mandatory. Please provide your name, email and password.",
     });
 
     return;
@@ -61,7 +61,7 @@ router.post("/signup", isLoggedOut, (req, res) => {
     .then((salt) => bcrypt.hash(password, salt))
     .then((hashedPassword) => {
       // Create a user and save it in the database
-      return User.create({ username, email, password: hashedPassword });
+      return User.create({ name, email, password: hashedPassword });
     })
     .then((user) => {
       res.redirect("/auth/login");
@@ -72,7 +72,7 @@ router.post("/signup", isLoggedOut, (req, res) => {
       } else if (error.code === 11000) {
         res.status(500).render("auth/signup", {
           errorMessage:
-            "Username and email need to be unique. Provide a valid username or email.",
+            "Name and email need to be unique. Provide a valid username or email.",
         });
       } else {
         next(error);
