831d94fc4f0dc951c4657e84eb02ef3a227b3cd0
RodriguezJoseMaria <143606586+RodriguezJoseMaria@users.noreply.github.com>
2024-03-17 12:48:38 +0100

    done

--- the-tarot-project/routes/dashboard.routes.js
+++ the-tarot-project/routes/dashboard.routes.js
@@ -1,15 +1,23 @@
-const express = require('express');
+const Card = require("../models/Card.model.js");
+const express = require("express");
 const router = express.Router();
 
 /* GET home page */
-router.get("/", (req, res, next) => {
+router.get("/", async (req, res, next) => {
   console.log(req.session);
 
   if (!req.session.currentUser) {
     return res.redirect("/auth/login");
   }
+  await Card.find().then((allCards) => {
+    // res.json(allCards);
+    res.render("dashboard/dashboard", {
+      user: req.session.currentUser,
+      allCards,
+    });
+  });
 
-  res.render("dashboard/dashboard", { user: req.session.currentUser });
+  // res.render("dashboard/dashboard", { user: req.session.currentUser });
 });
 
 module.exports = router;
