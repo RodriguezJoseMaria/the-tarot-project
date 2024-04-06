6a258f424e7cae17d2a25c627a4fb6b1ba45932e
RodriguezJoseMaria <143606586+RodriguezJoseMaria@users.noreply.github.com>
2024-04-06 16:34:57 +0200

    done

--- the-tarot-project/routes/cards.routes.js
+++ the-tarot-project/routes/cards.routes.js
@@ -1,77 +1,87 @@
 // routes/cards.routes.js
-const router = require('express').Router();
-const { getCards, createCard } = require('../controllers/cardController.js');
-const Card = require('../models/Card.model.js');
-const isLoggedIn = require('../middleware/isLoggedIn.js');
+const router = require("express").Router();
+const { getCards, createCard } = require("../controllers/cardController.js");
+const Card = require("../models/Card.model.js");
+const isLoggedIn = require("../middleware/isLoggedIn.js");
 
 // GET route to retrieve and display all the cards
-router.get('/', getCards);
+router.get("/", getCards);
 
-router.get('/create', isLoggedIn,  (req, res) => {
-  res.render('cards/cards-create');
+router.get("/create", isLoggedIn, (req, res) => {
+  res.render("cards/cards-create");
 });
 
-router.post('/create', createCard);
+router.post("/create", createCard);
 
-router.get('/cards-list', isLoggedIn, (req, res) => {
-  Card.find({user: req.session.currentUser._id}).then(cardFromDB => {
-  res.render('cards/cards-list', cardFromDB);
-});
+router.get("/cards-list", isLoggedIn, (req, res) => {
+  Card.find({ user: req.session.currentUser._id }).then((cardFromDB) => {
+    res.render("cards/cards-list", cardFromDB);
+  });
 });
 
-router.get('/:card/edit', isLoggedIn, (req, res) => {
+router.get("/:card/edit", isLoggedIn, (req, res) => {
   const { card } = req.params;
-  Card.findOne({ nameCard: card }).then(cardFromDB => {
+  Card.findOne({ nameCard: card }).then((cardFromDB) => {
     console.log(cardFromDB);
-    res.render('cards/cards-edit', cardFromDB);
+    res.render("cards/cards-edit", cardFromDB);
   });
 });
 
-router.post('/edit', isLoggedIn, (req, res) => {
-  const { id, categories, description } = req.body
-  Card.findByIdAndUpdate(id, { categories, description }, { new: true })
+router.post("/edit", isLoggedIn, (req, res) => {
+  const { id, categories, description } = req.body;
+  Card.findByIdAndUpdate(id, { categories, description }, { new: true }).then(
+    () => {
+      res.redirect("/dashboard");
+    }
+  );
+});
+
+router.get("/:card/delete", isLoggedIn, (req, res, next) => {
+  const { card } = req.params;
+  Card.findOneAndDelete({ nameCard: card })
     .then(() => {
-      res.redirect('/cards');
+      res.redirect("/cards");
     })
+    .catch((err) => next(err));
 });
 
-router.get('/:card/delete', isLoggedIn, (req, res, next) => {
+router.delete("/:card/delete", isLoggedIn, (req, res, next) => {
   const { card } = req.params;
   Card.findOneAndDelete({ nameCard: card })
     .then(() => {
-      res.redirect('/cards')
-    }).catch(err => next(err));
+      res.redirect("/cards");
+    })
+    .catch((err) => next(err));
 });
 
-router.delete('/:card/delete', isLoggedIn, (req, res, next) => {
-  const { card } = req.params;
-  Card.findOneAndDelete({ nameCard: card })
-    .then(() => {
-      res.redirect('/cards')
-    }).catch(err => next(err));
-});
-
-router.get('/:card', (req, res) => {
+router.get("/:card", (req, res) => {
   const { card } = req.params;
 
-  Card.findOne({ slug: card }).then(cardFromDB => {
-    res.render('cards/cards-create', cardFromDB);
+  Card.findOne({ slug: card }).then((cardFromDB) => {
+    res.render("cards/cards-create", cardFromDB);
   });
 });
 
-router.get('/:card/details', (req, res) => {
+router.get("/:card/details", (req, res) => {
   const { card } = req.params;
 
-  Card.findOne({ slug: card }).then(cardFromDB => {
-    res.render('cards/cards-details', cardFromDB);
+  Card.findOne({ slug: card }).then((cardFromDB) => {
+    res.render("cards/cards-details", cardFromDB);
   });
 });
 
-router.get('/:card', (req, res) => {
+router.get("/:card", (req, res) => {
   const { card } = req.params;
 
-  Card.findOne({ slug: card }).then(cardFromDB => {
-    res.render('cards/cards-create', cardFromDB);
+  Card.findOne({ slug: card }).then((cardFromDB) => {
+    res.render("cards/cards-create", cardFromDB);
+  });
+});
+
+router.get("/edit/:slug", (req, res) => {
+  const { slug } = req.params;
+  Card.findOne({ slug: slug }).then((cardFromDB) => {
+    res.render("cards/cards-edit", cardFromDB);
   });
 });
 
